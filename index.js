9df13b4342162e9e6851c24067b298c3dcbbde6e
SalehZo-Niloy <msnniloy7@gmail.com>
2022-11-08 05:55:54 +0600

    Server side initialized

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Orion Immigration & Visa Consultants Running');
+})
+
+app.listen(port, () => {
+    console.log('OIVC server running on port:', port);
+})
