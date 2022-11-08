f2e1cb631c2a827d6819d381e8a2c7e5d59a0bd3
SalehZo-Niloy <msnniloy7@gmail.com>
2022-11-08 17:30:23 +0600

    mongodb client initialized and dotenv used

--- index.js
+++ index.js
@@ -1,14 +1,40 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
+require('dotenv').config()
 
 app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.SECRET_USER}:${process.env.SECRET_PASSWORD}@cluster0.yarpj5v.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+const run = async () => {
+    try {
+        const serviceCollection = client.db('visa-immigration').collection('services');
+
+        app.get('/homeService', async (req, res) => {
+            const query = {};
+
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+    }
+    finally {
+
+    }
+}
+
+run().catch(e => console.error(e));
 
 app.get('/', (req, res) => {
-    res.send('Orion Immigration & Visa Consultants Running');
+    res.send('NVIS Running');
 })
 
 app.listen(port, () => {
-    console.log('OIVC server running on port:', port);
+    console.log('NVIS server running on port:', port);
 })
