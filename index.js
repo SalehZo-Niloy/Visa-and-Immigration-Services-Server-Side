25a20945ab38b7e17fb217440262ff4636385159
SalehZo-Niloy <msnniloy7@gmail.com>
2022-11-08 20:13:32 +0600

    specific service details api created

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 require('dotenv').config()
@@ -30,6 +30,16 @@ const run = async () => {
             const cursor = serviceCollection.find(query);
             const services = await cursor.toArray();
             res.send(services);
+        })
+
+        app.get('/serviceDetails/:id', async (req, res) => {
+            const id = req.params.id;
+            // console.log(id);
+
+            const query = { _id: ObjectId(id) };
+
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
         })
 
     }
